4457d286b669d87cf66ede9cc625092e3c4297ea
AryanMittal11 <mittalaryan1110@gmail.com>
2025-08-04 01:24:43 +0530

    socket-chat

--- server/routes/messageRouter.js
+++ server/routes/messageRouter.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { getMessages, getUsersForSidebar, markMessageAsSeen } from '../controllers/messageController.js';
+import { getMessages, getUsersForSidebar, markMessageAsSeen, sendMessage } from '../controllers/messageController.js';
 import { protectRoute } from '../middleware/auth.js';
 
 const messageRouter = express.Router();
@@ -7,5 +7,6 @@ const messageRouter = express.Router();
 messageRouter.get("/users", protectRoute, getUsersForSidebar)
 messageRouter.get("/:id", protectRoute, getMessages)
 messageRouter.put("mark/:id", protectRoute, markMessageAsSeen)
+messageRouter.post("/send/:id", protectRoute, sendMessage)
 
 export default messageRouter;
